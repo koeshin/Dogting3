bbc11c246ade33147bca7c002ac33dd754f711bd
koeshin <77971228+koeshin@users.noreply.github.com>
2023-05-29 18:34:40 +0900

    회원가입시 강아지 견종,나이,몸무게 받아서 파이어베이스에 저장

--- app/src/main/java/com/example/testfire/Friend.kt
+++ app/src/main/java/com/example/testfire/Friend.kt
@@ -3,6 +3,13 @@ package com.example.testfire
 data class Friend(
     val email : String? = null,
     val name : String? = null,
-    val profileImageUrl : String? = null,
-    val uid : String? = null
+    var profileImageUrl : String? = null,
+    val uid : String? = null,
+    val dog: Dog? = null
+)
+
+data class Dog(
+    val dclass: String? = null,
+    val dage: String? = null,
+    val dweight: String? = null
 )
--- app/src/main/java/com/example/testfire/RegistrationActivity.kt
+++ app/src/main/java/com/example/testfire/RegistrationActivity.kt
@@ -50,16 +50,23 @@ class RegistrationActivity: AppCompatActivity() {
         auth = Firebase.auth
         database = Firebase.database.reference
 
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            1
+        )
         val email = findViewById<EditText>(R.id.et_registration_id).text
         val password = findViewById<EditText>(R.id.et_registration_password).text
 
         val name = findViewById<EditText>(R.id.et_registration_name).text
         val button = findViewById<Button>(R.id.btn_registration)
+        val dclass = findViewById<EditText>(R.id.et_registration_dogclass).text
+        val dage = findViewById<EditText>(R.id.et_registration_dogage).text
+        val dweight = findViewById<EditText>(R.id.et_registration_dogweight).text
         val profile = findViewById<ImageView>(R.id.registration_iv)
         var profileCheck = false
 
-        profile.setOnClickListener{
+        profile.setOnClickListener {
             val intentImage = Intent(Intent.ACTION_PICK)
             intentImage.type = MediaStore.Images.Media.CONTENT_TYPE
             getContent.launch(intentImage)
@@ -69,15 +76,15 @@ class RegistrationActivity: AppCompatActivity() {
         val intent = Intent(this, LogInActivity2::class.java)
 
         button.setOnClickListener {
-            if(email.isEmpty() && password.isEmpty() && name.isEmpty() && profileCheck)  {
-                Toast.makeText(this, "아이디와 비밀번호, 프로필 사진을 제대로 입력해주세요.", Toast.LENGTH_SHORT).show()
-                Log.d("Email", "$email, $password")
-            }
+            // ... 이전 코드 생략 ...
 
-            else{
-                if(!profileCheck){
+            if (email.isEmpty() && password.isEmpty() && name.isEmpty() && dclass.isEmpty() && dweight.isEmpty() && dage.isEmpty() && profileCheck) {
+                Toast.makeText(this, "아이디와 비밀번호, 프로필 사진을 제대로 입력해주세요.", Toast.LENGTH_SHORT).show()
+                Log.d("Email", "$email, $password,$dclass,$dweight,$dweight")
+            } else {
+                if (!profileCheck) {
                     Toast.makeText(this, "프로필사진을 등록해주세요.", Toast.LENGTH_SHORT).show()
-                } else{
+                } else {
                     auth.createUserWithEmailAndPassword(email.toString(), password.toString())
                         .addOnCompleteListener(this) { task ->
                             if (task.isSuccessful) {
@@ -85,19 +92,47 @@ class RegistrationActivity: AppCompatActivity() {
                                 val userId = user?.uid
                                 val userIdSt = userId.toString()
 
+                                val dog = Dog(
+                                    dclass = dclass.toString(),
+                                    dage = dage.toString(),
+                                    dweight = dweight.toString()
+                                )
+
+                                // Friend 객체 생성 및 값을 설정합니다.
+                                val friend = Friend(
+                                    email = email.toString(),
+                                    name = name.toString(),
+                                    profileImageUrl = "",  // 프로필 이미지 URL은 이후에 설정됩니다.
+                                    uid = userIdSt,
+                                    dog = dog
+                                )
+
                                 FirebaseStorage.getInstance()
-                                    .reference.child("userImages").child("$userIdSt/photo").putFile(imageUri!!).addOnSuccessListener {
-                                        var userProfile: Uri? = null
-                                        FirebaseStorage.getInstance().reference.child("userImages").child("$userIdSt/photo").downloadUrl
-                                            .addOnSuccessListener {
-                                                userProfile = it
-                                                Log.d("이미지 URL", "$userProfile")
-                                                val friend = Friend(email.toString(), name.toString(), userProfile.toString(), userIdSt)
-                                                database.child("users").child(userId.toString()).setValue(friend)
+                                    .reference.child("userImages").child("$userIdSt/photo")
+                                    .putFile(imageUri!!)
+                                    .addOnSuccessListener { taskSnapshot ->
+                                        taskSnapshot.storage.downloadUrl
+                                            .addOnSuccessListener { uri ->
+                                                friend.profileImageUrl =
+                                                    uri.toString()  // 프로필 이미지 URL 설정
+                                                Log.d("이미지 URL", "${friend.profileImageUrl}")
+
+                                                // Friend 객체를 Firebase Realtime Database에 저장합니다.
+                                                database.child("users").child(userId.toString())
+                                                    .setValue(friend)
+                                                    .addOnSuccessListener {
+                                                        Log.d(
+                                                            "Firebase",
+                                                            "Friend 정보가 성공적으로 저장되었습니다."
+                                                        )
+                                                    }
+                                                    .addOnFailureListener {
+                                                        Log.d("Firebase", "Friend 정보 저장 실패: $it")
+                                                    }
                                             }
                                     }
+
                                 Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
-                                Log.e(TAG, "$userId")
                                 startActivity(intent)
                             } else {
                                 Toast.makeText(this, "등록에 실패했습니다.", Toast.LENGTH_SHORT).show()
@@ -107,7 +142,7 @@ class RegistrationActivity: AppCompatActivity() {
             }
         }
     }
-    public override fun onStart() {
+            public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
